efdb62b0bd1a39a86f230d27ab6803c6784da5ee
Sobhan Kazemi <sobhankazemi80@gmail.com>
2022-10-12 18:44:14 +0330

    solution to second part(adding customizable timer(via flag) to program)

--- main.go
+++ main.go
@@ -7,19 +7,37 @@ import (
 	"log"
 	"os"
 	"strings"
+	"time"
 )
 
-var fileName string
+var (
+	fileName          string
+	totalTime         int
+	score             int
+	numberOfQuestions int
+	lives             int
+)
 
 func init() {
 	flag.StringVar(&fileName, "file", "problems.csv", "questions starting file")
+	flag.IntVar(&totalTime, "time", 30, "total time for answering the questions")
+	flag.IntVar(&lives, "lives", 2, "number of lives in the game")
 	flag.Parse()
 }
 
 func main() {
+	fmt.Println("press any key to start")
+	var startPhrase string
+	fmt.Scanln(&startPhrase)
+	timer := time.NewTimer(time.Second * time.Duration(totalTime))
+	go func() {
+		<-timer.C
+		fmt.Printf("you scored %d out of %d\n", score, numberOfQuestions)
+		os.Exit(0)
+	}()
 	file, err := os.Open(fileName)
 	if err != nil {
-		log.Fatal("file does not exists")
+		log.Fatal("file does not exist")
 	}
 	defer file.Close()
 	problems := csv.NewReader(file)
@@ -27,19 +45,25 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	numberOfQuestions := len(records)
+	numberOfQuestions = len(records)
 	finished := true
-	var score int
 	for _, record := range records {
+		check := true
 		fmt.Printf("what is %s sir ?\n", record[0])
 		var answer string
 		fmt.Scan(&answer)
 		if strings.Compare(answer, record[1]) != 0 {
-			fmt.Printf("you answered %d out of %d questions\n", score, numberOfQuestions)
-			finished = false
-			break
+			lives--
+			if lives == 0 {
+				fmt.Printf("you scored %d out of %d\n", score, numberOfQuestions)
+				finished = false
+				break
+			}
+			check = false
 		}
-		score++
+		if check {
+			score++
+		}
 	}
 	if finished {
 		fmt.Println("congrats you answered all questions")
